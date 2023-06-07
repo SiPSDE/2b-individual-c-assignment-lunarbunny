69cec5953f97ed638b0dc10a8a610e74dc231106
lunarbunny <25877083+lunarbunny@users.noreply.github.com>
2023-06-07 16:26:37 +0800

    Submit assignment

--- assignment.c
+++ assignment.c
@@ -4,5 +4,45 @@
 #include <stdio.h>
 
 Node* readWords(char* filename) {
-    // Your implementation here
+    // Open the file
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Could not open file %s\n", filename);
+        return NULL;
+    }
+
+    Node* head = NULL;
+    Node* tail = NULL;
+    char word[100];
+
+    // Read words from the file, one line at a time
+    while (fgets(word, sizeof(word), file) != NULL) {
+        // Remove newline character, if present
+        size_t len = strlen(word);
+        if (len > 0 && word[len - 1] == '\n') {
+            word[len - 1] = '\0';
+        }
+
+        // Create a new node
+        Node* newNode = malloc(sizeof(Node));
+        if (newNode == NULL) {
+            printf("Error allocating memory\n");
+            return NULL;
+        }
+        newNode->word = strdup(word);  // You'll need to free this later!
+        newNode->next = NULL;
+
+        // Add the new node to the end of the linked list
+        if (head == NULL) {
+            head = newNode;
+        } else {
+            tail->next = newNode;
+        }
+        tail = newNode;
+    }
+
+    // Close the file
+    fclose(file);
+
+    return head;
 }
